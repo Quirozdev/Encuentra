4f40a4ccf97c282ac018423dce58718688a256f3
Daniel <daeb2222@gmail.com>
2024-02-11 19:43:37 -0700

    added svg file import, log in changes return button minor change pwdtxtinput changes

--- components/common/ReturnButton/ReturnButton.tsx
+++ components/common/ReturnButton/ReturnButton.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { TouchableOpacity,Image } from 'react-native';
 import styles from './returnButton.style';
+import BackArrow from '../../../assets/images/back_arrow.svg';
 
 const ReturnButton = () => {
     return (
         <TouchableOpacity style={styles.button}>
-            <Image source={require('../../../assets/images/back_arrow.png')} />
+            <BackArrow/>
         </TouchableOpacity>
     )
 }
---
+++ src/app/users/login/index.tsx
@@ -0,0 +1,6 @@
+import { SafeAreaView, Text } from "react-native";
+import LogInForm from "../../../../components/users/LogInForm/LogInForm";
+
+export default function LogIn() {
+    return (<LogInForm/>)
+}
