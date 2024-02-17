edb210ef715e8787a2c42b5e24f7732bbbf63d5b
Daniel <daeb2222@gmail.com>
2024-02-17 15:41:01 -0700

    resolviendo problemas merge login

--- components/users/LogInForm/LogInForm.style.ts
+++ components/users/LogInForm/LogInForm.style.ts
@@ -3,44 +3,43 @@ import { COLORS,FONTS,SIZES } from "../../../constants/theme";
 
 const styles = StyleSheet.create({
         container: {
-        flex: 1,
-        backgroundColor: COLORS.white,
-        padding: 25
-        },
-        subContainer: {
-        rowGap: 15,
-        marginHorizontal: 25
-        },
-        welcomeText: { 
-        fontSize: SIZES.xLarge,
-        fontFamily: FONTS.RubikBold,
-        color: COLORS.darkBlue,
-        marginTop: 25,
-        marginHorizontal: 25,
-        marginBottom: 35
-        },
-        baseInput: {
-        },
-        forgotPwdText: {
-        fontFamily: FONTS.RubikRegular,
-        fontSize: SIZES.small,
-        color: COLORS.grey,
-        textAlign: 'right',
-        marginRight: 25,
-        marginTop: 15,
-        marginBottom: 35
-        },
-        buttonContainer: {
-        width: '100%',
-        alignItems: 'center'
-        },
-        noAccountText: {
-        fontSize: SIZES.medium,
-        color: COLORS.grey,
-        position: 'absolute',
-        bottom: 32,
-        alignSelf: 'center',
-        }
+                flex: 1,
+                backgroundColor: COLORS.white,
+                },
+                subContainer: {
+                rowGap: 15,
+                marginHorizontal: 25
+                },
+                welcomeText: { 
+                fontSize: SIZES.xLarge,
+                fontFamily: FONTS.RubikBold,
+                color: COLORS.darkBlue,
+                marginTop: 25,
+                marginHorizontal: 25,
+                marginBottom: 35
+                },
+                baseInput: {
+                },
+                forgotPwdText: {
+                fontFamily: FONTS.RubikRegular,
+                fontSize: SIZES.small,
+                color: COLORS.grey,
+                textAlign: 'right',
+                marginRight: 25,
+                marginTop: 15,
+                marginBottom: 35
+                },
+                buttonContainer: {
+                width: '100%',
+                alignItems: 'center'
+                },
+                noAccountText: {
+                fontSize: SIZES.medium,
+                color: COLORS.grey,
+                position: 'absolute',
+                bottom: 32,
+                alignSelf: 'center',
+                }
 });
 
 export default styles;
